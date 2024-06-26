5139a600f729ef7b8c7edca0ce92db60c65f8812
mehmetbaba56 <143158362+mehmetbaba56@users.noreply.github.com>
2024-06-26 13:48:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SESSION = getenv("SESSION", )
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TSGCheckerChat")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TSGChecker")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6782067807").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
