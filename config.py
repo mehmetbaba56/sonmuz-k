5f67ecf3f14ff78597d2815b715f7df9055df571
mehmetbaba56 <143158362+mehmetbaba56@users.noreply.github.com>
2024-06-26 13:30:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION", BAE3xB4AHqae8q9OR2AIQRyzjpUW15N0AjkayWmH65dC97LR_D8w005l7NfeAZdz3XD-OlGJEbBvUDux57qu5V2WYOTMofMvznUKEoXmcyRc2Pz2_Gong_UUYZLvKcfNNsGOYN3kmxtQgO0oyog7rKAIiivvEAAG6uoVkra-FG30a7gkQy6Z_s1nmmas8iVEPeJCRwbLe7kQZQlC58IpTS9qTbnDx31T8bWX6CZzvvwHVbxkgjU-w-GURh3wKGlbFtv0gz-4vIedanzL_TencSzscsKB7RSLp_kfOHv0-v66Io6w-RFaH3g-IZSuA8e_4UXmKD5i6NfC1hATvgUTse9xz48d4AAAAAGdozS4AA)
+SESSION = getenv("SESSION", )
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TSGCheckerChat")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TSGChecker")
